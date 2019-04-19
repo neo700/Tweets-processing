6315a07be03d19b38e055a51396ce45a86838a62
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2019-04-19 20:56:30 +0530

    Update twe_geo.py

--- twe_geo.py
+++ twe_geo.py
@@ -4,7 +4,7 @@ access_token = ""
 access_token_secret = ""
 consumer_key = ""
 consumer_secret = ""
-
+#authorisation
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
